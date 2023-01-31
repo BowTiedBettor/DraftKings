07037e21f0e3adf87f6e1398b7dc63a249ee4872
BowTiedBettor <109925930+BowTiedBettor@users.noreply.github.com>
2023-02-01 00:02:21 +0100

    Update draftkings_script.py

--- draftkings_script.py
+++ draftkings_script.py
@@ -1,9 +1,19 @@
 from draftkings_class import DraftKings
+import json
 
-dk = DraftKings(league = "NHL")
+"""
+Create a DraftKings class object
+"""
+dk = DraftKings(league = "Portugal - Primeira Liga")
 
-games = dk.get_pregame_odds()
+"""
+Find all games & their event_ids
+"""
+game_ids = dk.get_event_ids()
+for game, event_id in game_ids.items():
+    print(game, event_id)
 
-dk.store_as_json()
-
-
+"""
+Set up a stream awaiting odds updates for the Moneyline market [all games]
+"""
+dk.live_odds_stream(markets = ['Moneyline'])
