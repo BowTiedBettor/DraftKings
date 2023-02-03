dc4a5d577168fc7d197612f037b5669f5369fc52
BowTiedBettor <109925930+BowTiedBettor@users.noreply.github.com>
2023-02-03 21:53:11 +0100

    Update exceptions.py

--- exceptions.py
+++ exceptions.py
@@ -4,10 +4,12 @@ class ScraperException(Exception):
     """
     pass
 
+
 class WSEventTypeException(ScraperException):
     """
     ...
     """
+
     def __init__(self, event: str):
         self.event = event
 
@@ -19,16 +21,19 @@ class EventIdException(ScraperException):
     """
     ...
     """
+
     def __init__(self, event_id: str):
         self.event_id = event_id
 
     def __str__(self):
         return (f"Wrong event_id: {self.event_id}")
 
+
 class MarketException(ScraperException):
     """
     ...
     """
+
     def __init__(self, market: str):
         self.market = market
 
