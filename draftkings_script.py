2c6101301ea2dc0df2963f83020218eadcfe4a92
BowTiedBettor <109925930+BowTiedBettor@users.noreply.github.com>
2023-02-05 15:37:45 +0100

    Update draftkings_script.py

--- draftkings_script.py
+++ draftkings_script.py
@@ -13,7 +13,7 @@ for game, event_id in game_ids.items():
     print(game, event_id)
 
 """
-Set up a stream awaiting odds updates for the Moneyline market [all games]
+Set up a stream awaiting odds updates for the Moneyline market
 """
 dk.live_odds_stream(
     event_ids=["28335346", "28335344", "28335347"], markets=['Moneyline'])
